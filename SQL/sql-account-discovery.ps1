7f28a76e481dc023e215658717dc091c00f59767
afalahi <ali@afalahi.com>
2018-05-02 16:32:19 -0400

    added a filtering at the wmi call level. modified several things on the sql query. rearranged code blocks

--- SQL/sql-account-discovery.ps1
+++ SQL/sql-account-discovery.ps1
@@ -1,85 +1,84 @@
 $computerName = $args[0]
+function Invoke-SqlCommand() {
+    [cmdletbinding(DefaultParameterSetName="integrated")]
+    Param (
+        [Parameter(Mandatory=$true)][Alias("Serverinstance")][string]$Server,
+        [Parameter(Mandatory=$false)][string]$Database="master",
+        [Parameter(Mandatory=$true, ParameterSetName="not_integrated")][string]$Username,
+        [Parameter(Mandatory=$true, ParameterSetName="not_integrated")][string]$Password,
+        [Parameter(Mandatory=$false, ParameterSetName="integrated")][switch]$UseWindowsAuthentication,
+        [Parameter(Mandatory=$false)][int]$CommandTimeout=0
+    )
+    #build connection string
+    $connstring = "Server=$Server; Database=$Database; "
+    If ($PSCmdlet.ParameterSetName -eq "not_integrated") {
+        $connstring += "User ID=$Username; Password=$Password;"
+    }
+    elseif ($PSCmdlet.ParameterSetName -eq "integrated") {
+        $connstring += "Trusted_Connection=Yes; Integrated Security=SSPI;"
+    }
+    #connect to database
+    $connection = New-Object System.Data.SqlClient.SqlConnection($connstring)
+    $connection.Open()
+    #build query object
+    $command = $connection.CreateCommand()
+    $query = "select loginname, dbname 
+        from $Database..syslogins 
+        where hasaccess = 1 
+        and [password] is not null 
+        and (sysadmin = 1 or securityadmin = 1 or serveradmin = 1) 
+        and isntname = 0"
+    $command.CommandText = $query  
+    $command.CommandTimeout = $CommandTimeout
+    #run query
+    $adapter = New-Object System.Data.SqlClient.SqlDataAdapter $command
+    $dataset = New-Object System.Data.DataSet
+    $adapter.Fill($dataset) | out-null
+    #return the first collection of results or an empty array
+    If ($dataset.Tables[0] -ne $null) {
+        $table = $dataset.Tables[0]
+    }
+    elseif ($table.Rows.Count -eq 0) {
+        $table = New-Object System.Collections.ArrayList
+    }
+    $connection.Close()
+    return $table
+}#end function
 #check for SQL server
-try
-    {
-        $getSqlService = Get-WmiObject -Class win32_service -ComputerName $computerName -ErrorAction Stop| Where {$_.Name -Like "*MSSQL*"}
+try {
+    $sqlService = @(Get-WmiObject -Class win32_service -ComputerName $computerName -Filter {DisplayName LIKE 'SQL Server (%'} -ErrorAction Stop)
+}
+catch [System.Runtime.InteropServices.COMException] {
+    Write-Debug "The computer '$computerName' does not exist or is inaccessible"
+    Write-Debug "Exception Message: $($_.exception.message)"
+    Write-Debug "Stacktrace: $($_.exception.stacktrace)"
+    throw "Error connecting to $computerName : $($_.exception.message)"
+}
+if($sqlService.Count -ne 0) {
+    try {
+    $sqlService.ForEach({
+        if ($_.Name -ne 'MSSQLSERVER') {
+            $instanceName=$_.Name.Split('$')[1]
+            $computerName+="\"+$instanceName
+        }
+    });
+        $users = @()    
+        $table = Invoke-SqlCommand -Server $computerName -UseWindowsAuthentication
+        $table.forEach({
+            $object = "" | Select-Object Machine, UserName, Database, Enabled
+            $object.Machine = $computerName;
+            $object.UserName = $_.loginname;
+            $object.Database = $_.dbname;
+            $object.Enabled = $true;
+            $users +=$object
+        });
+        return $users
     }
-catch [System.Runtime.InteropServices.COMException]
-    {
-        Write-Debug "The computer '$computerName' does not exist or is inaccessible"
-        Write-Debug "Exception Message: $($_.exception.message)"
-        Write-Debug "Stacktrace: $($_.exception.stacktrace)"
+    catch {
+        throw $("SQL error: $($_.Exception.message)")
     }
-if($getSqlService -ne $NULL)
-    {
-        function Invoke-SqlCommand() 
-            {
-                [cmdletbinding(DefaultParameterSetName="integrated")]
-                Param 
-                    (
-                        [Parameter(Mandatory=$true)][Alias("Serverinstance")][string]$Server,
-                        [Parameter(Mandatory=$true)][string]$Database,
-                        [Parameter(Mandatory=$true, ParameterSetName="not_integrated")][string]$Username,
-                        [Parameter(Mandatory=$true, ParameterSetName="not_integrated")][string]$Password,
-                        [Parameter(Mandatory=$false, ParameterSetName="integrated")][switch]$UseWindowsAuthentication = $true,
-                        [Parameter(Mandatory=$true)][string]$Query,
-                        [Parameter(Mandatory=$false)][int]$CommandTimeout=0
-                    )
-                #build connection string
-                $connstring = "Server=$Server; Database=$Database; "
-                If ($PSCmdlet.ParameterSetName -eq "not_integrated") { $connstring += "User ID=$Username; Password=$Password;" }
-                ElseIf ($PSCmdlet.ParameterSetName -eq "integrated") { $connstring += "Trusted_Connection=Yes; Integrated Security=SSPI;" }
-                #connect to database
-                $connection = New-Object System.Data.SqlClient.SqlConnection($connstring)
-                $connection.Open()
-                #build query object
-                $command = $connection.CreateCommand()
-                $command.CommandText = $Query
-                $command.CommandTimeout = $CommandTimeout
-                #run query
-                $adapter = New-Object System.Data.SqlClient.SqlDataAdapter $command
-                $dataset = New-Object System.Data.DataSet
-                $adapter.Fill($dataset) | out-null
-                #return the first collection of results or an empty array
-                If ($dataset.Tables[0] -ne $null) {$table = $dataset.Tables[0]}
-                ElseIf ($table.Rows.Count -eq 0) { $table = New-Object System.Collections.ArrayList }
-                $connection.Close()
-                return $table
-            }
 
-        $sql = "select * from master..syslogins where hasaccess = 1 and [password] is not null and (sysadmin = 1 or securityadmin = 1 or serveradmin = 1)"
-        $database = "master"
-        try 
-            {
-                $targetInstance = $computerName
-                if ($getSqlService.Name -ne 'MSSQLSERVER')
-                    {
-                        $sqlService=$getSqlService.Name.Split('$')[1]
-                        $targetInstance+="\"+$sqlService
-                    }
-                $table = Invoke-SqlCommand -Server $targetInstance  -Database $database -Query $sql -UseWindowsAuthentication
-                $users = @()
-                foreach ($user in $table) 
-                    {
-                        $accountName =$user.loginname
-                        $object = New-Object PSObject;
-                        $object | Add-Member -MemberType NoteProperty -Name Machine -Value $targetInstance;
-                        $object | Add-Member -MemberType NoteProperty -Name UserName -Value $accountName;
-                        $object | Add-Member -MemberType NoteProperty -Name Database -Value 'master';
-                        $object | Add-Member -MemberType NoteProperty -Name Enabled -Value  $true;
-                        $users +=$object
-                    }
-
-                return $users
-            }
-        catch 
-            {
-                Write-Debug ("Can't open connection: {0}`n{1}" -f `
-                $server, $_.Exception.ToString())
-            }
-
-    }
-else
-    {
-        throw "No SQL Accounts Found on Computer"
-    }
+}
+else {
+    throw "No SQL Accounts Found on Computer"
+}
