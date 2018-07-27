0c5704fcac7a35435358b91104a3dec7915aa3e7
Ali Falahi <ali@afalahi.com>
2018-07-27 16:21:56 -0400

    updated script logic

--- Oracle/Oracle-discovery-script.ps1
+++ Oracle/Oracle-discovery-script.ps1
@@ -1,70 +1,115 @@
 #path to DLL
 try{
-$dll = "Oracle.ManagedDataAccess.dll" 
-$directoryName=(Get-ChildItem -Path "C:\oracle" -Include $dll -Recurse).DirectoryName
-$path = $directoryName+"\"+$dll
-Add-Type -Path $path
+    $OMDA = "Oracle.ManagedDataAccess.dll" 
+    $directoryName=(Get-ChildItem -Path "C:\oracle" -Include $OMDA -Recurse).DirectoryName
+    $path =$("$directoryName\$OMDA")
+    Add-Type -Path $path
 }
 
-catch 
-{
-    throw "An Error occured loading the DLL: {0} make sure you install the oracle driver or point to the oracle path" -f $_
+catch {
+    throw "An Error occured loading the Oracle Data Provider: $($_.Exception.Message)"
 }
 
-$username = $args[0]
-$password = $args[1]
-$server = $args[2]
-<#$Sid = ""
-$serviceName=""
-$connectionString = "Data Source=(DESCRIPTION=(Address_List=(ADDRESS=(PROTOCOL=TCP)(HOST=$server)(PORT=1521)))(CONNECT_DATA=(SID=$Sid))); User Id=$username; Password=$password;"#>
-$connectionString = "Data Source=(DESCRIPTION=(Address_List=(ADDRESS=(PROTOCOL=TCP)(HOST=$server)(PORT=1521)))); User Id=$username; Password=$password;"
-#create the connection
-    try
-        {
+Function Get-OracleAccounts{
+    Param (
+        [string]
+        $UserName,
+        [string]
+        $Password,
+        [string]
+        $ComputerName,
+        [string]
+        $ServiceName
+    )
+    process{
+        $connectionString =
+@"
+Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$ComputerName)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=$ServiceName)));User Id="$UserName";Password=$Password
+"@
+        try{
             $connection = New-Object Oracle.ManagedDataAccess.Client.OracleConnection($connectionString)
             $connection.open()
         }
-
-    catch [Oracle.ManagedDataAccess.Client.OracleException]
-        {
-            Write-Debug $Error[0].Exception.Message
-	          throw "An Error occured openning connection to the database: {0} please check your connection string, and try again" -f $_
+        catch [Oracle.ManagedDataAccess.Client.OracleException]{
+	        throw "Connection error: $($_.Exception.Message)"
         }
-
-#run the query
-    try
-        {
-            $query = "SELECT USERNAME FROM DBA_USERS WHERE ACCOUNT_STATUS = 'OPEN'"
+        try{
+            $query = "SELECT * FROM DBA_ROLE_PRIVS WHERE GRANTED_ROLE = 'DBA'"
             $command=$connection.CreateCommand()
             $command.CommandText=$query
             $command.CommandTimeout=0
             $dataAdapter=New-Object Oracle.ManagedDataAccess.Client.OracleDataAdapter $command
             $dataSet = New-Object System.Data.DataSet
             $dataAdapter.Fill($dataSet) | Out-Null
-            $users= $dataSet.Tables[0]
-
-            #Build the user object
-            $oracleUsers = @()
-            foreach ($user in $users)
-                {
-                      $usrObj= New-Object -TypeName PSObject
-                      $usrObj| Add-Member -MemberType NoteProperty -Name Server -Value $connection.HostName
-                      $usrObj| Add-Member -MemberType NoteProperty -Name Port -Value "1521"
-                      $usrObj| Add-Member -MemberType NoteProperty -Name Database -Value $connection.DatabaseName
-                      $usrObj| Add-Member -MemberType NoteProperty -Name Username -Value $user.USERNAME
-                      $usrObj| Add-Member -MemberType NoteProperty -Name Enabled -Value $true
-                      $oracleUsers +=$usrObj
-                  }
-                return $oracleUsers
-        }
-
-    catch [Oracle.ManagedDataAccess.Client.OracleException]
-        {
-            Write-Debug $Error[0].Exception.Message
-            throw "An Error occured running the query or building the user object: {0}" -f $_
-        }
-
-    finally
-            {
-                $connection.Dispose()
+            if($dataSet.Tables[0] -ne $null){
+                $table= $dataSet.Tables[0]
             }
+            else {
+                $table = New-Object System.Collections.ArrayList
+            }
+            return $table
+        }
+        catch [Oracle.ManagedDataAccess.Client.OracleException]{
+            throw "An Error occured running the query$($_.Exception.Message)"
+        }
+        finally {
+            $connection.Close()
+            $connection.Dispose()
+        }
+    }
+}
+##
+##
+#Start an SSH session to get the oracle instances
+##
+##
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $args[0] ,$(ConvertTo-SecureString -AsPlainText $args[1] -Force)
+try{
+    $sshSession = New-SSHSession -ComputerName $args[2] -Credential $credential -Port 22 -Force -ErrorAction Stop
+}
+catch{
+    throw "Error connecting to $($args[2]): $($_.Exception.Message)"
+}
+try {
+    $command =Invoke-SSHCommand -Command "ps -ef |grep pmon" -SSHSession $sshSession
+    Remove-SSHSession -SSHSession $sshSession | Out-Null
+}
+catch {
+    throw $_.Exception.Message
+}
+$serviceNames=@($command.OutPut.forEach({
+    if($_ -like "*oracle*"){
+        $_.SubString($_.LastIndexOf("_")+1)
+    }
+});)
+##
+##
+#Build the user object
+##
+##
+$Accounts = @()
+if($serviceNames.Count -ne 0){
+    try {
+        $serviceNames.ForEach({
+            $serviceName = $_
+            $results= @(Get-OracleAccounts -UserName $args[3] -Password $args[4] -ComputerName $args[2] -ServiceName $serviceName)
+            $results.ForEach({
+                $usrObj= "" | Select-Object Machine, UserName, Role, Database,Port, Enabled
+                $usrObj.Machine = $args[2]
+                $usrObj.Port = "1521"
+                $usrObj.Database = $serviceName
+                $usrObj.UserName = $_.GRANTEE
+                $usrObj.Role = $_.GRANTED_ROLE
+                $usrObj.Enabled = $true
+                $Accounts +=$usrObj
+            });
+        });
+        return $Accounts
+    }
+    catch {
+        throw $_.Exception.Message
+    }
+}
+else {
+    throw "No Oracle instances running on machine"
+}
