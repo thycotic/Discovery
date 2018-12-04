246dd892f954d4989dae52cc24b3898cddcba662
Cody Hussey <cody.hussey@thycotic.com>
2018-12-04 08:51:35 -0500

    Latest Updates

--- Oracle/Oracle-discovery-script.ps1
+++ Oracle/Oracle-discovery-script.ps1
@@ -1,4 +1,14 @@
+# Thycotic Extensible Discovery - Oracle
+
 # Test access to DLL
+$LinuxUser = $args[0]
+$LinuxPass = $args[1]
+$Target = $args[2]
+$OracleUser = $args[3]
+# Only necessary if using hyphen in username...
+# $OracleUser = $OracleUser.Trim("`"")
+$OraclePass = $args[4]
+
 try{
     $OMDA = "Oracle.ManagedDataAccess.dll" 
     $directoryName=(Get-ChildItem -Path "C:\oracle" -Include $OMDA -Recurse).DirectoryName
@@ -27,54 +37,62 @@ Function Get-OracleAccounts{
 @"
 Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$ComputerName)(PORT=$Port))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=$ServiceName)));User Id="$UserName";Password=$Password
 "@
-        try{
+            try{
             $connection = New-Object Oracle.ManagedDataAccess.Client.OracleConnection($connectionString)
+            Write-Debug "opening oracle connection $ConnectionString"
             $connection.open()
-        }
-        catch [Oracle.ManagedDataAccess.Client.OracleException]{
-	        throw "Connection error: $($_.Exception.Message)"
-        }
-        try{
-            $query = "SELECT * FROM DBA_ROLE_PRIVS WHERE GRANTED_ROLE = 'DBA'"
-            $command=$connection.CreateCommand()
-            $command.CommandText=$query
-            $command.CommandTimeout=0
-            $dataAdapter=New-Object Oracle.ManagedDataAccess.Client.OracleDataAdapter $command
-            $dataSet = New-Object System.Data.DataSet
-            $dataAdapter.Fill($dataSet) | Out-Null
-            if($dataSet.Tables[0] -ne $null){
-                $table= $dataSet.Tables[0]
+                
+                try{
+                        Write-Debug "Opened oracle connection"
+                        $query = "SELECT * FROM DBA_ROLE_PRIVS WHERE GRANTED_ROLE = 'DBA'"
+                        $command=$connection.CreateCommand()
+                        $command.CommandText=$query
+                        $command.CommandTimeout=0
+                        $dataAdapter=New-Object Oracle.ManagedDataAccess.Client.OracleDataAdapter $command
+                        $dataSet = New-Object System.Data.DataSet
+                        $dataAdapter.Fill($dataSet) | Out-Null
+                        if($dataSet.Tables[0] -ne $null){
+                            $table= $dataSet.Tables[0]
+                        }
+                        else {
+                            $table = New-Object System.Collections.ArrayList
+                        }
+                        return $table
+                    }   
+            catch [Oracle.ManagedDataAccess.Client.OracleException]{
+                throw "An Error occured running the query: $($_.Exception.Message)"
             }
-            else {
-                $table = New-Object System.Collections.ArrayList
-            }
-            return $table
         }
-        catch [Oracle.ManagedDataAccess.Client.OracleException]{
-            throw "An Error occured running the query$($_.Exception.Message)"
-        }
-        finally {
-            $connection.Close()
-            $connection.Dispose()
-        }
-    }
+         catch{
+             write-debug "Connection error: $($_.Exception.Message)"
+
+         }   
+
+     }
+    
+            
 }
 
 # Grab Ports for Oracle Listeners
-
-$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $args[0], $(ConvertTo-SecureString -AsPlainText $args[1] -Force)
+Import-Module -Name Posh-SSH
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $LinuxUser, $(ConvertTo-SecureString -AsPlainText $LinuxPass -Force)
 #$credential = Get-Credential
 try{
-    $sshSession = New-SSHSession -ComputerName $args[2] -Credential $credential -Port 22 -Force -ErrorAction Stop
+    $sshSession = New-SSHSession -ComputerName $Target -Credential $credential -Port 22 -Force -ErrorAction Stop
+ 
 }
 catch{
-    throw "Error connecting to $($args[2]): $($_.Exception.Message)"
+    throw "Error connecting to $($Target): $($_.Exception.Message)"
 }
 try {
     # REPLACE home path of Discovery User Account - D
-    $command =Invoke-SSHCommand -Command "/home/thyco/discover.sh" -SSHSession $sshSession
+    Set-SCPFile -ComputerName $Target -Credential $credential -Port 22 -LocalFile "C:\Users\svc-Thycoticadm\discover.sh" -RemotePath "/home/svc-thycoadm/" -Force -NoProgress
+    $command =Invoke-SSHCommand -Command "chmod +x /home/svc-thycoadm/discover.sh" -SSHSession $sshSession
+    $command =Invoke-SSHCommand -Command "/home/svc-thycoadm/discover.sh" -SSHSession $sshSession
     # This will grab the file which is created by bash script on linux system
-    Get-SCPFile -ComputerName $args[2] -Credential $credential -Port 22 -RemoteFile "/home/thyco/oracleinfo.txt" -LocalFile "oracleinfo.txt"
+    Get-SCPFile -ComputerName $Target -Credential $credential -Port 22 -RemoteFile "/home/svc-thycoadm/oracleinfo.txt" -LocalFile "C:\Users\svc-Thycoticadm\oracleinfo.txt" -Force -NoProgress
+    $command =Invoke-SSHCommand -Command "rm /home/svc-thycoadm/discover.sh" -SSHSession $sshSession
+    $command =Invoke-SSHCommand -Command 'rm /home/svc-thycoadm/oracleinfo.txt' -SSHSession $sshSession
     Remove-SSHSession -SSHSession $sshSession | Out-Null
 }
 catch {
@@ -84,27 +102,32 @@ catch {
 # BUILD arrays of the connection details from oracleinfo file
 $services=@()
 $ports=@()
-$oraconnections = Get-Content "oracleinfo.txt"
+$oraconnections = Get-Content "C:\users\svc-Thycoticadm\oracleinfo.txt"
 foreach ($line in $oraconnections){$ports+= $line.Split(" ")[0]}
 foreach ($line in $oraconnections){$services+= $line.Split(" ")[1]}
 
+# DEBUG ONLY...
+#$ports >> 'C:\users\svc-thycoticadm\portlog.txt'
+#$services >> 'C:\users\svc-thycoticadm\servicelog.txt'
+
 #Build the user object
 $Accounts = @()
-if($services.Count -ne 0){
+if($services){
     try {
         $s = 0
         $services.ForEach({
             $serviceName = $services[$s]
             $ServicePort = $ports[$s]
-            $results= @(Get-OracleAccounts -UserName $args[3] -Password $args[4] -ComputerName $args[2] -ServiceName $serviceName -Port $ServicePort)
+            $results= @(Get-OracleAccounts -erroraction silentlycontinue -UserName $OracleUser -Password $OraclePass -ComputerName $Target -ServiceName $serviceName -Port $ServicePort)
             $results.ForEach({
-                $usrObj= "" | Select-Object Machine, UserName, Role, Database,Port, Enabled
-                $usrObj.Machine = $args[2]
-                $usrObj.Port = $ServicePort
-                $usrObj.Database = $serviceName
-                $usrObj.UserName = $_.GRANTEE
-                $usrObj.Role = $_.GRANTED_ROLE
-                $usrObj.Enabled = $true
+               $usrObj= New-Object -TypeName psobject 
+                $usrObj | Add-Member -MemberType NoteProperty -Name Machine -Value $Target
+                $usrObj | Add-Member -MemberType NoteProperty -Name Port -Value $ServicePort
+                $usrObj | Add-Member -MemberType NoteProperty -Name Database -Value $serviceName
+                $usrObj | Add-Member -MemberType NoteProperty -Name UserName -Value $_.GRANTEE
+                $usrObj | Add-Member -MemberType NoteProperty -Name Role -Value $_.GRANTED_ROLE
+                $usrObj | Add-Member -MemberType NoteProperty -Name Enabled -Value $true
+
                 $Accounts +=$usrObj
             });
             $s++
